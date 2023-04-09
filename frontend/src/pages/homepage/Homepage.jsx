0d698038b050e9eabec2d199f9187352213f75bd
“tcrz” <tonybaidoo0@gmail.com>
2023-04-09 21:29:35 +0000

    homepage update

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -1,19 +1,23 @@
 
 import { Link } from "react-router-dom";
 import useVerifyAuth from "../../hooks/useVerifyAuth";
+import { Button } from "flowbite-react";
 
 const Homepage = () => {
   useVerifyAuth()
   return (
-    <div className="h-screen bg-dark-blue flex flex-col justify-around items-center">
-      <div className="text-center">
-        <h1 className="mb-10 block text-3xl font-bold text-yellow-400">
-          Welcome
+    <div className="h-screen bg-gray-100 flex flex-col justify-around items-center">
+      <div className="flex flex-col items-center text-center borrder">
+        <h1 className="mb-10 block text-5xl font-bold text-blue-500">
+          taskManager
         </h1>
-        <Link to="/sign-in" className="underline text-white">
-          {" "}
-          Proceed to Sign In
-        </Link>
+        
+          <Link to="/sign-in" className="border text-white">
+            <Button className="text-blue-500">
+              {" "}
+              Proceed to Sign In
+            </Button>
+          </Link>
       </div>
     </div>
   );
